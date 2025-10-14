62b608d816a5c91683d89ce2bb8c30d254e1e29d
jihoon <wlgnstls0413@naver.com>
2025-10-14 22:36:27 +0900

    feat: 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import java.io.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("덧셈함 문자열을 입력해 주세요.");
+
+        String str = br.readLine();
+        System.out.println(str);
     }
 }
