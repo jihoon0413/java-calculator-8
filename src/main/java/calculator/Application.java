9ec5a87e4375519e0f6fb3684278aa117e2e9b5a
jihoon <wlgnstls0413@naver.com>
2025-10-15 09:54:37 +0900

    feat: calculator 실행 및 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,26 @@
 package calculator;
 
-import java.io.*;
+import calculator.utils.Calculator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Application {
     public static void main(String[] args) throws IOException {
+
+        Calculator calculator = new Calculator();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("덧셈함 문자열을 입력해 주세요.");
 
         String str = br.readLine();
-        System.out.println(str);
+
+        int ans = calculator.calc(str);
+
+        System.out.println("결과 : " + ans);
+
     }
+
+
 }
